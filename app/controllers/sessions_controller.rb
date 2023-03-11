a3b9534c939034d6c6536f70bee518d879f40e9b
saadshahab1 <saad.shahab@kellogg.northwestern.edu>
2023-03-11 08:07:19 +0000

    up to hashed passwords

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,25 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"email" => params["email"]})
+    # if user exists
+    if @user
+      # if user's password matches submitted password
+        if @user["password"] == params["password"]
+          # send them to places
+          flash["notice"] = "You logged in!"
+          redirect_to "/places"
+        else
+          # otherwise, back to login
+          flash["notice"] = "Nope!"
+          redirect_to "/sessions/new"
+        end
+    else 
+      # otherwise, back to login
+      redirect_to "/sessions/new"
+    end
   end
-
-  def destroy
-  end
+  #def destroy
+  #end
 end
   
