18f3559396b93a8017333db92a41562e73bfc344
saadshahab1 <saad.shahab@kellogg.northwestern.edu>
2023-03-11 18:25:12 +0000

    latest

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,16 +5,29 @@ class PostsController < ApplicationController
     @post.place_id = params["place_id"]
   end
 
+  #def create
+    #@post = Post.new
+    #@post["title"] = params["post"]["title"]
+    #@post["description"] = params["post"]["description"]
+    #@post["posted_on"] = params["post"]["posted_on"]
+    #@post["place_id"] = params["post"]["place_id"]
+    #@post.uploaded_image.attach(params["post"]["uploaded_image"])
+    #@post["user_id"] = @current_user["id"] 
+   # @post.save
+    #redirect_to "/places/#{@post["place_id"]}"
+  #end
+
   def create
-    @post = Post.new
-    @post["title"] = params["post"]["title"]
-    @post["description"] = params["post"]["description"]
-    @post["posted_on"] = params["post"]["posted_on"]
-    @post["place_id"] = params["post"]["place_id"]
-    @post.uploaded_image.attach(params["post"]["uploaded_image"])
-    @post["user_id"] = @current_user["id"] 
-    @post.save
-    redirect_to "/places/#{@post["place_id"]}"
+    if @current_user
+      @post = Post.new
+      @post["body"] = params["post"]["body"]
+      @post.uploaded_image.attach(params["post"]["uploaded_image"])
+      @post["user_id"] = @current_user["id"]
+      @post.save
+    else
+      flash["notice"] = "Login first."
+    end
+    redirect_to "/posts"
   end
 
 end
